5e92fdb3bc80c22729095cc8cb47378fdddc7f11
im_tk2nd-tmp@yahoo.co.jp <master-yoda>
2019-02-16 07:26:36 +0000

    [2019/02/16 07:25:58] 基本メニュー作成

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+
+  def access
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-#  get '/', to: 'home#index'
-  get root to: 'home#index'
+  get root to: "static_pages#top"
+  get "top", to: "static_pages#top"
+  get "about", to: "static_pages#about"
+  get "access", to: "static_pages#access"
+  get "contact", to: "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
